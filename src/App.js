e0cb476797ba7555d6db9738eebbd5c10f2c679a
Ostara <71931135+edurrada@users.noreply.github.com>
2021-05-03 13:22:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+export default function App() {
+return (
+<div class="App">
+<body>
+
+  <div class="block">
+    <div class="container">
+      <h2>Get <span class="destaque" >insights</span> that help your business grow.</h2>
+      <div class="description">
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience, and overall efficiency.
+      </div>
+
+      <ul>
+        <li><h1>10k+</h1>Companies</li>
+        <li><h1>314</h1>Templates</li>
+        <li><h1>12M+</h1>Queries</li>
+      </ul>
+    </div>
+
+      <img src="https://exame.com/wp-content/uploads/2017/07/reuniacc83o.png"/>
+      <div></div>
+  </div> 
+  
+
+
+
+</body>
+</div>
+);
+}
